8f3b5a9d5fa97d20ab8c7d29aaa8622ebba26fc7
Miguel Martin <miguel.martin@piksel.com>
2020-12-17 18:09:37 +0100

    fix lint warnings

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ class App extends React.Component<Props, State> {
         this.lottolandService = new Lottoland();
     }
 
-    public componentDidMount() {
+    public componentDidMount(): void {
         this.lottolandService.get('20200904').then((data: any) => {
             this.setState({ data });
             console.log('data', data);
@@ -46,49 +46,3 @@ class App extends React.Component<Props, State> {
 }
 
 export default App;
-
-// import React from 'react';
-// import './App.styles.less';
-
-// import Lottoland from './services/Api/Lottoland';
-
-// interface Props {}
-
-// interface State {
-//     data: any;
-// }
-
-// class App extends React.Component<Props, State> {
-//     private lottolandService: Lottoland;
-
-//     public constructor(props: Props) {
-//         super(props);
-//         this.state = {
-//             data: {},
-//         };
-//         this.lottolandService = new Lottoland();
-//     }
-
-//     // public componentDidMount() {
-//     //     this.lottolandService.get('20200904').then((result: any) => console.log('result', result));
-//     // }
-
-//     public render(): React.ReactElement {
-//         const value = 'Showcase App!!!';
-
-//         return (
-//             <div className="main">
-//                 <input
-//                     value={value}
-//                     onChange={(event) => {
-//                         console.log('event', event.currentTarget.value);
-//                     }}
-//                 />
-//                 Hello world
-//                 <pre>{this.state.data}</pre>
-//             </div>
-//         );
-//     }
-// }
-
-// export default App;
