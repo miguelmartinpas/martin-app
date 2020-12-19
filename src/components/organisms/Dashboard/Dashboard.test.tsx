044743fc8dc9b5d1644fdb78ebb38709c7a3247f
Miguel Martin <miguel.martin@piksel.com>
2020-12-19 22:21:56 +0100

    Improve UT for Dashboard

--- src/components/organisms/Dashboard/Dashboard.test.tsx
+++ src/components/organisms/Dashboard/Dashboard.test.tsx
@@ -6,6 +6,17 @@ import Dashboard from './index';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const mockGet = jest.fn().mockImplementation(() => Promise.resolve({}));
+jest.mock('../../../services/Api/Lottoland', () => {
+    class MockLottoland {
+        // eslint-disable-next-line class-methods-use-this
+        public get(value: string) {
+            return mockGet();
+        }
+    }
+    return MockLottoland;
+});
+
 describe('Dashboard Component', () => {
     let component: ReactWrapper;
 
@@ -18,6 +29,21 @@ describe('Dashboard Component', () => {
             renderComponent();
 
             expect(component).toBeDefined();
+        });
+
+        it('WHEN Dropdown > onchange is called THEN mockGet should be called', () => {
+            renderComponent();
+
+            const event = {
+                currentTarget: {
+                    value: 'foo-bar',
+                },
+            } as React.ChangeEvent<HTMLInputElement>;
+
+            const dropdownComponentProps = component.find('Dropdown').at(0).props();
+            const { onChange = jest.fn() } = dropdownComponentProps;
+            onChange(event);
+            expect(mockGet).toHaveBeenCalled();
         });
     });
 });
